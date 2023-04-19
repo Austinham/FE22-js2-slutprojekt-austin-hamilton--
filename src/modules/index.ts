e74c7ca12af9ef6d23a6b85c77266912f39e63e4
austin <austin-hamilton17@hotmail.com>
2023-04-19 12:46:05 +0200

    kill me

---
+++ src/modules/index.ts
@@ -0,0 +1,152 @@
+export const url: string = `https://login-2c7ee-default-rtdb.europe-west1.firebasedatabase.app/`;
+const baseurl: string = `${url}users.json`;
+console.log(baseurl);
+
+//login with email and password selection
+const existingName = document.querySelector('#loginUserName') as HTMLInputElement ;
+const existingPassword = document.querySelector('#loginUserPassword') as HTMLInputElement ;
+const loginButton = document.querySelector('#loginBtn') as HTMLElement;
+const signedInLink = document.querySelector('#loginLink') as HTMLAnchorElement;
+
+//create account button
+const makeAccount = document.querySelector('#createAccount') as HTMLElement;
+//register selections for password/username
+const registerName = document.querySelector('#signup-Name') as HTMLInputElement;
+const registerPassword = document.querySelector('#signup-Password') as HTMLInputElement ;
+const registerAccount = document.querySelector('#createAccountLink') as HTMLInputElement;
+
+const avatarChoices = document.querySelectorAll('.avatar') as NodeListOf<HTMLInputElement>;
+// styles selection
+
+const registerForm = document.querySelector('#createAccountForm') as HTMLElement;
+const loginForm = document.querySelector('#login') as HTMLElement;
+
+registerForm.style.display = 'none';
+
+// Register the user
+const pickAvatar: string [] = [];
+
+avatarChoices.forEach((avatar) =>{
+  avatar.addEventListener('click',() => {
+    pickAvatar.shift();
+    pickAvatar.push(avatar.value)
+  })
+})
+
+signedInLink.addEventListener('click',(e) => {
+  e.preventDefault();
+  loginForm.style.display = 'block';
+  registerForm.style.display = 'none';
+
+})
+
+registerAccount.addEventListener('click', (e) => {
+  e.preventDefault();
+  loginForm.style.display = 'none';
+  registerForm.style.display = 'block';
+});
+
+
+//make account 
+
+makeAccount?.addEventListener('click', maketheAccount)
+
+async function maketheAccount(e: Event): Promise<void> {
+  e.preventDefault();
+
+  const registerName = document.querySelector('#signup-Name') as HTMLInputElement;
+  const registerPassword = document.querySelector('#signup-Password') as HTMLInputElement;
+  const registerImage = document.querySelector('#image-section') as HTMLInputElement;
+
+  const userNameInput: string = registerName.value;
+  const passwordInput: string = registerPassword.value;
+
+
+
+  const newAccount = {
+    username: userNameInput,
+    password: passwordInput,
+    image: pickAvatar[0]
+  };
+
+  const init = {
+    method: 'POST',
+    body: JSON.stringify(newAccount),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8'
+    }
+  };
+
+  const response = await fetch(baseurl, init);
+  if (response.status === 200 || response.status === 201) {
+    // Account created successfully
+    console.log('Account created successfully');
+    // Show a success message to the user
+    const successMessage = document.createElement('div');
+    successMessage.innerText = 'Your account has been created successfully!';
+    successMessage.style.color = 'green';
+    const registerForm = document.querySelector('#createAccountForm') as HTMLElement;
+    registerForm.parentElement?.insertBefore(successMessage, registerForm.nextSibling);
+  } else {
+    // Account creation failed
+    console.log('Account creation failed');
+    // Show an error message to the user
+    const errorMessage = document.createElement('div');
+    errorMessage.innerText = 'There was an error creating your account. Please try again later.';
+    errorMessage.style.color = 'red';
+    const registerForm = document.querySelector('#createAccountForm') as HTMLElement;
+    registerForm.parentElement?.insertBefore(errorMessage, registerForm.nextSibling);
+  }
+}
+
+
+//check if user exists and is authenticated
+loginButton.addEventListener('click', loginVerification);
+
+async function loginVerification(event: Event): Promise<void> {
+  event.preventDefault();
+  
+
+  try {
+      const response = await fetch(baseurl);
+      const data = await response.json();
+      verifyProcess(data);
+  } catch (error) {
+      console.log(error);
+  }
+}
+
+//  function verifyProcess(data: { [key: string]: { username: string, password: string, image: string } }): void {
+// const listFromData: { username: string, password: string, image: string }[] = Object.values(data);
+// console.log(listFromData,'this is the list from data');
+// const userNameInput: string = (document.querySelector('#loginUserName') as HTMLInputElement).value;
+// const passwordInput: string = (document.querySelector('#loginUserPassword') as HTMLInputElement).value;
+// console.log(data,'2321');
+// for (let i = 0; i < listFromData.length; i++) {
+//     if (userNameInput === listFromData[i].username && passwordInput === listFromData[i].password) {
+//       localStorage.setItem('loggedInUser', JSON.stringify(listFromData[i]));
+//         window.location.assign('../html/signin.html');
+//         console.log('Welcome');
+//         return;
+//     }
+// }
+// alert('User not found.');
+// }
+function verifyProcess(data: { [key: string]: { username: string, password: string, image: string } }): void {
+  const listFromData: { id: string, username: string, password: string, image: string }[] = Object.entries(data).map(([id, user]) => ({ id, ...user }));
+  console.log(listFromData,'this is the list from data');
+  const userNameInput: string = (document.querySelector('#loginUserName') as HTMLInputElement).value;
+  const passwordInput: string = (document.querySelector('#loginUserPassword') as HTMLInputElement).value;
+  console.log(data,'2321');
+  for (let i = 0; i < listFromData.length; i++) {
+      if (userNameInput === listFromData[i].username && passwordInput === listFromData[i].password) {
+        localStorage.setItem('loggedInUser', JSON.stringify(listFromData[i]));
+          window.location.assign('src/html/signin.html');
+          console.log(listFromData[i].id,'Welcome');
+          return;
+      }
+  }
+  alert('User not found.');
+}
+
+
---
+++ src/modules/userpage.ts
@@ -0,0 +1,179 @@
+const url: string = `https://login-2c7ee-default-rtdb.europe-west1.firebasedatabase.app/`;
+const loggedInUserJSON: string | null = localStorage.getItem("loggedInUser");
+const loggedInUser = loggedInUserJSON ? JSON.parse(loggedInUserJSON) : null;
+const newUrl = loggedInUser ? `${url}users/${loggedInUser.id}.json` : null;
+console.log(loggedInUser.id);
+
+console.log(loggedInUser.username, "2");
+
+const postButton = document.querySelector("#post-button") as HTMLButtonElement;
+const deleteUserBtn = document.querySelector(
+  "#dele-button"
+) as HTMLButtonElement;
+const postInput = document.querySelector("#status") as HTMLInputElement;
+const container = document.querySelector("#container") as HTMLDivElement;
+const textContent = document.querySelector("#content") as HTMLDivElement;
+const post = document.querySelector("#post") as HTMLDivElement;
+
+postButton.addEventListener("click", updatePost); // add event listener to call updatePost() function
+
+deleteUserBtn.addEventListener("click", () => {
+  if (newUrl) {
+    const init = {
+      method: "DELETE",
+    };
+    fetch(newUrl, init)
+      .then((response) => {
+        console.log(response.status);
+        localStorage.clear();
+        window.location.assign("./src/index.html"); // reload the page to clear data and redirect to login page
+      })
+      .catch((error) => {
+        console.log(`Error deleting user account: ${error}`);
+        alert("Error deleting user account. Please try again later.");
+      });
+  }
+});
+async function updatePost(e: Event): Promise<void> {
+  e.preventDefault();
+
+  const writemessage: string = postInput.value;
+  console.log(writemessage);
+
+  if (loggedInUser && loggedInUser.id) {
+    const newUrl = `${url}users/${loggedInUser.id}.json`;
+    const response = await fetch(newUrl);
+    const userData = (await response.json()) as { posts?: string[] };
+    console.log(userData, "posts");
+
+    if (userData !== null && userData.posts !== undefined) {
+      userData.posts.push(writemessage);
+    } else if (userData !== null) {
+      userData.posts = [writemessage];
+    } else {
+      // handle case where userData is null or undefined
+    }
+
+    const init = {
+      method: "PATCH",
+      body: JSON.stringify(userData),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    };
+
+    const updateResponse = await fetch(newUrl, init);
+    const dataFromPost = (await updateResponse.json()) as { posts?: string[] };
+    console.log(dataFromPost, "hello ew world!");
+  }
+  await fetchData();
+  
+}
+
+// displaying the posts
+async function fetchData(): Promise<void> {
+  if (loggedInUser && loggedInUser.id) {
+    const usersUrl = `${url}users.json`;
+    try {
+      const response = await fetch(usersUrl);
+      const data = await response.json();
+
+      if (data) {
+        const user = data[loggedInUser.id];
+        if (user && user.posts) {
+          const userPosts = user.posts
+            .map((post) => `<p><strong>You:</strong> ${post}</p>`)
+            .join("");
+          post.innerHTML = userPosts;
+        } else {
+          console.log(`Error: no posts found for user ${loggedInUser.id}.`);
+        }
+      } else {
+        console.log("Error: data is null or undefined.");
+      }
+    } catch (error) {
+      console.log(`Error fetching data from ${usersUrl}: ${error}`);
+    }
+  }
+}
+fetchData();
+
+async function fetchingAlldata() {
+  const newUrl = `${url}users.json`;
+  const response = await fetch(newUrl);
+  const showDataFromProfile = await response.json() as Record<string, User>;
+  showOtherProfiles(showDataFromProfile);
+  console.log(showDataFromProfile);
+}
+fetchingAlldata();
+interface UserProfile {
+  username: string;
+  password: string;
+  image: string;
+  posts?: string[];
+}
+
+function showOtherProfiles(showDataFromProfile: Record<string, UserProfile>) {
+  const keysfromUsernameData = Object.keys(showDataFromProfile);
+  const objectFromUsernameData = Object.values(showDataFromProfile);
+
+  const modal = document.getElementById("myModal") as HTMLDivElement;
+  const modalContent = document.getElementById("modal-content") as HTMLDivElement;
+  const closeBtn = document.getElementsByClassName("close")[0];
+  
+
+  for (let i = 0; i < objectFromUsernameData.length; i++) {
+    const userProfile = objectFromUsernameData[i];
+    const usernameProfile = document.createElement('h4');
+    const postsProfile = document.createElement('h4');
+    const imageProfile = document.createElement('img'); // create image element
+    document.body.appendChild(usernameProfile);
+    document.body.appendChild(postsProfile);
+    usernameProfile.innerText = userProfile.username;
+    imageProfile.src = userProfile.image; // set the image URL
+
+    usernameProfile.addEventListener('click', () => {
+      const latestPost = userProfile.posts && userProfile.posts.length > 0 ? userProfile.posts[userProfile.posts.length - 1] : '';
+      const listFromData: UserProfile & { id: string, latestPost: string } = { ...userProfile, id: keysfromUsernameData[i], latestPost };
+      console.log(listFromData);
+      localStorage.setItem('loggedInUser', JSON.stringify(listFromData));
+      modalContent.innerHTML = "";
+      const usernameWithLatestPost = document.createElement('p');
+      usernameWithLatestPost.innerText = `${userProfile.username} - Latest Post: ${latestPost}`;
+      modalContent.appendChild(usernameWithLatestPost);
+      modalContent.appendChild(imageProfile); // append the image element to the modal content
+      modal.style.display = "block";
+    });
+  }
+
+  closeBtn.addEventListener('click', () => {
+    modal.style.display = "none";
+  });
+}
+
+
+
+
+interface User {
+  username: string;
+  password: string;
+  image: string;
+}
+
+if (loggedInUserJSON) {
+  const loggedInUser: User = JSON.parse(loggedInUserJSON);
+  const username = loggedInUser.username;
+  const image = loggedInUser.image;
+  
+  const usernameElement = document.querySelector("#name") as HTMLElement;
+
+  console.log(usernameElement)
+  const imageElement = document.querySelector("#images") as HTMLImageElement;
+
+  usernameElement.textContent = username;
+  imageElement.src = image;
+  console.log(username, "hello");
+} else {
+  // handle case where loggedInUserJSON is null or undefined
+}
+
